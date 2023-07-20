629410cf07b9f82fa62d3cc146c3f78ef59141c7
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-07-19 21:33:55 -0300

    :lipstick: feat: update

--- profile/src/styles/themes/defaultTheme.ts
+++ profile/src/styles/themes/defaultTheme.ts
@@ -1,4 +1,16 @@
 export const defaultTheme = {
+  COLORS: {
+    ACQUA_GREEN: {
+      "900": "#153438",
+      "800": "#2b6f77",
+      "700": "#368d97",
+      "600": "#40acb7",
+      "500": "#5bbcc7"
+    },
+    WHITE: {
+      "100": "#FFFFFF"
+    }
+  },
   DEVICE: {
     min: `(min-width: 375px)`,
     mobile: `(min-width: 425px)`,
