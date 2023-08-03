8cb815237edd56c3337917d1effc6c4073d169d0
Cleiton barros <ton.barros123@hotmail.com>
2023-08-02 22:12:17 -0300

    :lipstick: style

--- profile/src/styles/global.ts
+++ profile/src/styles/global.ts
@@ -1,30 +1,34 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-    *{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        text-decoration: none !important;
-        list-style: none;
-        outline: none;
-        scroll-behavior:smooth;
-    }
+  *{
+     margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+    list-style: none;
+    outline: none;
+    scroll-behavior:smooth;
+  }
+  a{
+    color: #000;
+  }
 
-    body{
-        font-family: 'Poppins', sans-serif;
-        -webkit-font-smoothing: antialiased;
-    }
+  body{
+    font-family: 'Fira Sans', sans-serif;
+    overflow-y: hidden;
+    -webkit-font-smoothing: antialiased;
+  }
 
-    html ::-webkit-scrollbar {
-        width: 6px;
-    }
+  html ::-webkit-scrollbar {
+    width: 6px;
+  }
 
-    html ::-webkit-scrollbar-thumb {
-      background: black;
-    }
+  html ::-webkit-scrollbar-thumb {
+  background: black;
+  }
     
-    html ::-webkit-scrollbar-track {
-      background: white;
-    }
+  html ::-webkit-scrollbar-track {
+    background: white;
+  }
 `;
