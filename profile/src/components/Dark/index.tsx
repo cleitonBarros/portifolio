8163a3d48b269699d48cc12bffea32600cb058d0
Cleiton barros <ton.barros123@hotmail.com>
2023-08-07 16:12:03 -0300

    :recycle: refactor:  Dark componenent

--- profile/src/components/Dark/index.tsx
+++ profile/src/components/Dark/index.tsx
@@ -1,31 +1,19 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 
 import { DarkModeContext } from "../../context/useDarkMode";
 import * as S from "./style";
 
 export function Dark() {
   const { changeTheme } = useContext(DarkModeContext);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [menuState, toggleMenu] = useState<boolean>(true);
-
-  function handleToggleMenu() {
-    toggleMenu(menuState);
-  }
-
-  function handleChangeTheme() {
-    changeTheme();
-    handleToggleMenu();
-  }
-
   return (
     <S.Container className="animate__animated animate__fadeInDown">
       <input type="radio" name="mode" id="dark" />
-      <label onClick={handleChangeTheme} title="dark mode" htmlFor="dark">
+      <label onClick={changeTheme} title="dark mode" htmlFor="dark">
         Dark
       </label>
       <S.Line></S.Line>
       <input type="radio" name="mode" id="light" />
-      <label onClick={handleChangeTheme} title="light mode" htmlFor="light">
+      <label onClick={changeTheme} title="light mode" htmlFor="light">
         Light
       </label>
     </S.Container>
