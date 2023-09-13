bea466d6d872e877aa581cca565dae453205b9bf
Cleiton barros <ton.barros123@hotmail.com>
2023-09-12 22:57:00 -0300

    :recycle: refactor: Refactor code, delte Hamburger button, add SiderMenu component

--- profile/src/components/Header/index.tsx
+++ profile/src/components/Header/index.tsx
@@ -1,45 +1,18 @@
 import "animate.css";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 
-import { DarkModeContext } from "../../context/useDarkMode";
 import { LanguageContext } from "../../context/useLanguage";
 import { Dark } from "../Dark";
-import { Menu } from "../Menu";
+import { SiderMenu } from "../SideMenu";
 import * as S from "./style";
-
-import { Spin as Hamburger } from "hamburger-react";
 
 export function Header() {
   const useLanguage = useContext(LanguageContext);
-  const { darkMode } = useContext(DarkModeContext);
-  const [isOpen, setOpen] = useState<boolean>(false);
   const Langue = useLanguage?.currentLanguage;
-  const [isScroll, setIsScroll] = useState<boolean>(false);
-
-  <Hamburger
-    onToggle={(toggled) => {
-      if (toggled) {
-        setOpen(true);
-        return;
-      } else {
-        setOpen(false);
-      }
-    }}
-  />;
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 50) {
-        setIsScroll(true);
-        return;
-      } else {
-        setIsScroll(false);
-      }
-    });
-  }, [isScroll]);
 
   return (
     <>
-      <S.Header className={isScroll ? "scrolled" : ""}>
+      <S.Header>
         <S.Nav>
           <h1 title="Logo">
             <a className="logo " href="#">
@@ -47,26 +20,18 @@ export function Header() {
             </a>
           </h1>
           <Dark />
-
           <ul>
             <li title="translate">
               <p onClick={useLanguage?.handleLanguageChange}>
                 {Langue === "pt" ? "PT" : "EN"}
               </p>
             </li>
-            <li title="menu">
-              <Hamburger
-                toggled={isOpen}
-                toggle={setOpen}
-                color={darkMode === "dark" ? "#00D2C1" : "#F31559"}
-                size={44}
-                duration={0.5}
-              />
+            <li className="menu">
+              <SiderMenu />
             </li>
           </ul>
         </S.Nav>
       </S.Header>
-      <Menu Open={isOpen} close={setOpen} />
     </>
   );
 }
